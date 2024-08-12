71018515d8dbcc495f87a1ded357b94835ff8bd4
dannn <akbar.zakki@muf.co.id>
2024-08-12 11:42:32 +0700

    memasukan comment createwrapper

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,94 +1,94 @@
-import { ConfigProvider } from "antd";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import NProgress from "nprogress";
-import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import cookie from "react-cookies";
-import { useAsync } from "react-use";
+import { ConfigProvider } from 'antd'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
+import cookie from 'react-cookies'
+import { useAsync } from 'react-use'
 // import { SessionProvider } from "next-auth/react";
 // import { Provider } from "next-auth/client"
 
 // components
-import MainLayout from "../components/Layout/MainLayout";
-import Loading from "../components/Loading";
+import MainLayout from '../components/Layout/MainLayout'
+import Loading from '../components/Loading'
 // constants
-import { validateMessages } from "../constants/validateMessages";
+import { validateMessages } from '../constants/validateMessages'
 // store
-import wrapperStore from "../redux";
+import wrapperStore from '../redux'
 // utils
-import AuthStorage from "../utils/auth-storage";
+import AuthStorage from '../utils/auth-storage'
 
-require("../styles/index.less");
+require('../styles/index.less')
 
 const urlsIgnore = [
-  "/forgot-password",
-  "/login-first",
-  "/login",
-  "/sign-up",
-  "/verify-email",
-  "/reset-password",
-];
+  '/forgot-password',
+  '/login-first',
+  '/login',
+  '/sign-up',
+  '/verify-email',
+  '/reset-password',
+]
 
 const MyApp = (props) => {
-  const router = useRouter();
-  const { token } = router?.query || {};
-  const { Component, pageProps } = props;
-  const [awaitLoading, setAwaitLoading] = useState(true);
-  const Layout = Component.Layout || MainLayout;
+  const router = useRouter()
+  const { token } = router?.query || {}
+  const { Component, pageProps } = props
+  const [awaitLoading, setAwaitLoading] = useState(true)
+  const Layout = Component.Layout || MainLayout
 
   useEffect(() => {
     const handleRouteChange = (url, { shallow }) => {
       if (!shallow) {
-        NProgress.start();
+        NProgress.start()
       }
-    };
+    }
 
-    router.events.on("routeChangeStart", handleRouteChange);
-    router.events.on("routeChangeComplete", () => NProgress.done());
-    router.events.on("routeChangeError", () => NProgress.done());
+    router.events.on('routeChangeStart', handleRouteChange)
+    router.events.on('routeChangeComplete', () => NProgress.done())
+    router.events.on('routeChangeError', () => NProgress.done())
 
     // If the component is unmounted, unsubscribe
     // from the event with the off method:
     return () => {
-      router.events.off("routeChangeStart", handleRouteChange);
-      router.events.off("routeChangeComplete", () => NProgress.done());
-      router.events.off("routeChangeError", () => NProgress.done());
-    };
+      router.events.off('routeChangeStart', handleRouteChange)
+      router.events.off('routeChangeComplete', () => NProgress.done())
+      router.events.off('routeChangeError', () => NProgress.done())
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   const requestNotificationPermission = async () => {
-    const permission = await window.Notification.requestPermission();
+    const permission = await window.Notification.requestPermission()
     // value of permission can be 'granted', 'default', 'denied'
     // granted: user has accepted the request
     // default: user has dismissed the notification permission popup by clicking on x
     // denied: user has denied the request.
-    if (permission !== "granted") {
-      throw new Error("Permission not granted for Notification");
+    if (permission !== 'granted') {
+      throw new Error('Permission not granted for Notification')
     }
-  };
+  }
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", async function () {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', async function () {
         try {
           // window.swRegistration = await navigator.serviceWorker.register(
           //   "/sw.js"
           // );
-          const permission = await requestNotificationPermission();
+          const permission = await requestNotificationPermission()
           window.showLocalNotification = (title, options) => {
-            window.swRegistration.showNotification(title, options);
-          };
+            window.swRegistration.showNotification(title, options)
+          }
         } catch (err) {
-          console.log("Service Worker registration failed: ", err);
+          console.log('Service Worker registration failed: ', err)
         }
-      });
+      })
     }
-  }, []);
+  }, [])
 
   useAsync(async () => {
-    setAwaitLoading(false);
-  }, []);
+    setAwaitLoading(false)
+  }, [])
 
   // useAsync(async () => {
   // 	if(token && !AuthStorage.validToken(token)) {
@@ -111,8 +111,8 @@ const MyApp = (props) => {
     <Layout>
       <Head>
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, height=device-height, user-scalable=0"
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no, height=device-height, user-scalable=0'
         />
       </Head>
       {/* <Provider session={pageProps.session}>
@@ -123,47 +123,47 @@ const MyApp = (props) => {
       </ConfigProvider>
       <Loading fullScreen loading={awaitLoading} />
     </Layout>
-  );
-};
+  )
+}
 
 MyApp.getInitialProps = async (context) => {
-  const { ctx, Component } = context;
+  const { ctx, Component } = context
 
   if (!process.browser) {
-    cookie.plugToRequest(ctx.req, ctx.res);
+    cookie.plugToRequest(ctx.req, ctx.res)
   }
 
   //if (AuthStorage.loggedIn && urlsIgnore.includes(ctx.pathname)) {
   if (AuthStorage.loggedIn) {
     if (ctx.res) {
-      ctx.res.writeHead(302, { Location: "/" });
-      ctx.res.end();
+      ctx.res.writeHead(302, { Location: '/' })
+      ctx.res.end()
     }
   }
 
   // calls page's getInitialProps and fills appProps.pageProps
-  let pageProps = {};
+  let pageProps = {}
 
   if (Component?.getInitialProps) {
-    pageProps = await Component?.getInitialProps(ctx);
+    pageProps = await Component?.getInitialProps(ctx)
   }
 
   const propsData = {
     ...pageProps,
-  };
+  }
 
-  let layoutProps = {};
+  let layoutProps = {}
 
   if (Component?.Layout) {
     layoutProps = await Component?.Layout?.getInitialProps?.({
       ...ctx,
       pageProps: propsData,
-    });
+    })
   } else {
     layoutProps = await MainLayout?.getInitialProps?.({
       ...ctx,
       pageProps: propsData,
-    });
+    })
   }
 
   return {
@@ -171,12 +171,26 @@ MyApp.getInitialProps = async (context) => {
       ...propsData,
       ...layoutProps,
     },
-  };
-};
+  }
+}
 
 MyApp.propTypes = {
   Component: PropTypes.node,
   pageProps: PropTypes.object,
-};
+}
 
-export default wrapperStore.withRedux(MyApp);
+export default wrapperStore.withRedux(MyApp)
+/*Function createWrapper umumnya digunakan dalam file _app.js di Next.js untuk mengintegrasikan store Redux dengan aplikasi Next.js secara global. Hal ini penting karena _app.js merupakan file khusus di Next.js yang digunakan untuk mengatur Layout dan global behavior dari aplikasi, termasuk konfigurasi Redux.
+
+Beberapa alasan mengapa createWrapper digunakan di _app.js adalah sebagai berikut:
+
+Global State Management: Dengan menggunakan Redux dan next-redux-wrapper di _app.js developer dapat memastikan bahwa store Redux yang sama digunakan di seluruh aplikasi. Ini memungkinkan komponen mana pun di aplikasi untuk mengakses state global dan memperbarui state ketika diperlukan.
+
+Efficient Store Initialization: Inisialisasi store Redux yang efisien dapat dilakukan sekali di _app.js dan digunakan di semua halaman. Ini mencegah pengulangan kode dan memastikan konsistensi dalam manajemen state aplikasi.
+
+Next.js Middleware: next-redux-wrapper menyediakan integrasi yang baik dengan middleware Next.js seperti getInitialProps atau getServerSideProps. Ini memungkinkan aplikasi untuk melakukan pembaruan server-side secara efisien dan mengelola state di sisi klien dengan baik.
+
+Debugging dan Development: Dengan mengatur debug: true di createWrapper, Anda dapat dengan mudah mengakses informasi debug di mode pengembangan. Ini bermanfaat ketika Anda perlu melacak perubahan state atau perilaku aplikasi saat mengembangkan fitur baru.
+
+Dengan mengintegrasikan store Redux menggunakan createWrapper di _app.js, developer dapat memanfaatkan kekuatan Redux untuk manajemen state yang kuat dan integrasi yang baik dengan Next.js, sehingga memudahkan dalam pengembangan dan pemeliharaan aplikasi yang lebih besar dan kompleks. 
+*/
